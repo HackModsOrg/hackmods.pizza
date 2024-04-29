9b9b6010af25276f8eeed0f1fa2bee579801269e
Bassusteur <42449683+bassusteur@users.noreply.github.com>
2024-04-29 13:01:29 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,11 @@
 let title = "hackmods.pizza";
 let i = 0;
 
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
 function funnywrite(text) {
   while(i<text.length) {
   setTimeout(document.write(text.charAt(i)), 500);
   i++;
   } 
 }
+
+funnywrite(title);
