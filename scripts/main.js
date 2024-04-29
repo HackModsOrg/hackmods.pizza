56fe74c56152991185e1ca81073ca9a8a8564101
Bassusteur <42449683+bassusteur@users.noreply.github.com>
2024-04-29 13:45:28 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,10 @@ let title = "Hackmods.pizza";
 function funnywrite(destination,text,speed) {
     var i = 0;
     var interval = setInterval(function(){
+        if (i > text.length){
+            clearInterval(interval);
+            document.getElementById(destination).innerHTML = "";
+        }
         document.getElementById(destination).innerHTML += text.charAt(i);
         i++;
         if (i > text.length){
