9a8a10cafd33e17fa388523e044ad4dd7647eac7
Bassusteur <42449683+bassusteur@users.noreply.github.com>
2024-04-30 16:27:58 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 let title = "ackmods.pizza";
-let paragraph = "pizzapizzapizzapizzapizzapizzapizzapizzapizzapizzapizzapizzapizzapizzapizza";
+let paragraph = "pizza";
 
 function funnywrite(destination,text,speed) {
     document.getElementById(destination).innerHTML = "H";
@@ -13,10 +13,21 @@ function funnywrite(destination,text,speed) {
     }, speed);
 }
 
+function hellwrite(destination,text,speed) {
+    document.getElementById(destination).innerHTML = "";
+    var i = 0;
+    while(1):
+        var interval = setInterval(function(){
+            document.getElementById(destination).innerHTML += text.charAt(i);
+            i++;
+            if (i > text.length){
+                clearInterval(interval);
+            }
+        }, speed);
+}
+
 window.addEventListener('load', function () {
     funnywrite("title",title,100);
     funnywrite("p1",paragraph,50);
-    funnywrite("p2",paragraph,50);
-    funnywrite("p3",paragraph,50);
     setInterval(funnywrite,3000,"title",title,100);
 })
