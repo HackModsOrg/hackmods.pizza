98098f0be12fda3efea3d68f70962463860f15ab
Bassusteur <42449683+bassusteur@users.noreply.github.com>
2024-04-30 20:09:14 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -14,14 +14,17 @@ function funnywrite(destination,text,speed) {
 }
 
 window.addEventListener('load', function () {
-    window.onblur = function () {
-   
-   // tab is changed
-    };
-    window.onfocus = function () {
-    funnywrite("title",title,100);
-    funnywrite("p1",paragraph,50);
-    setInterval(funnywrite,3000,"title",title,100);   
-   // tab is active
-    };
+    document.addEventListener("visibilitychange", () => {
+   if (document.hidden) {
+      
+      // tab is changed
+   } else {
+        funnywrite("title",title,100);
+        funnywrite("p1",paragraph,50);
+        setInterval(funnywrite,3000,"title",title,100);   
+      // tab is active
+   }
+});
+
+
 })
