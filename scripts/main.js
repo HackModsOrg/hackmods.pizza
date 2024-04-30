90ad1a9564a86612d7ca315945266116bb081a6d
Bassusteur <42449683+bassusteur@users.noreply.github.com>
2024-04-30 22:10:54 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -17,12 +17,4 @@ function funnywrite(destination,text,speed) {
 window.addEventListener('load', function () {      
     funnywrite("title",title,100);
     funnywrite("p1",paragraph,50);
-    if (!document.hidden) {
-        var textint = setInterval(funnywrite,3000,"title",title,100);
-    }
-    else
-    {
-        clearInterval(textint);
-        document.getElementById("title").innerHTML = "\u200c";
-    }
 })
