0084a3cb54e337af9f160b7434d922108b5d562d
LinaLinn <lina.cloud@outlook.de>
2024-05-01 07:30:15 +0200

    Use the destinations element to get the next letter

--- scripts/main.js
+++ scripts/main.js
@@ -2,18 +2,17 @@ let title = "Hackmods.pizza";
 let paragraph = "pizza";
 
 function funnywrite(destination,text,speed) {
-    var i = 0;
     var interval = setInterval(function(){
-        document.getElementById(destination).innerHTML += text.charAt(i);
-        i++;
-        if (b.length > text.length){
+        var destinationElement = document.getElementById(destination)
+        destinationElement.innerHTML += text.charAt(destinationElement.innerHTML.length);
+        if (destinationElement.innerHTML.length >= text.length){
             clearInterval(interval);
         }
     }, speed);
-    document.getElementById(destination).innerHTML = "\u200c";
+    //document.getElementById(destination).innerHTML = "\u200c";
 }
 
-window.addEventListener('load', function () {      
+window.addEventListener('load', function () { 
     funnywrite("title",title,100);
     funnywrite("p1",paragraph,50);
 })
